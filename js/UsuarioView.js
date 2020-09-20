621318110eb62e3128bfedce1621e714c9471d90
Elton <eltonrazzini@hotmail.com>
2020-09-20 14:40:13 -0300

    inserir Bootstrap na api

--- js/UsuarioView.js
+++ js/UsuarioView.js
@@ -1,18 +1,18 @@
 function exibirUsuarios(Usuarios){
-var usuario = Usuarios[0];
+ var usuario = Usuarios[0];
 
-/*
-var eleCodigo = document.getElementById('codigo');
-eleCodigo.innerHTML =usuario.codigo;
-var eleNome = document.getElementById('nome');
-eleNome.innerHTML =usuario.nome;
-var eleLogin = document.getElementById('login');
-eleLogin.innerHTML =usuario.login;
-var eleEmail = document.getElementById('email');
-eleEmail.innerHTML =usuario.email;
-*/
+  /*
+    var eleCodigo = document.getElementById('codigo');
+    eleCodigo.innerHTML =usuario.codigo;
+    var eleNome = document.getElementById('nome');
+    eleNome.innerHTML =usuario.nome;
+    var eleLogin = document.getElementById('login');
+    eleLogin.innerHTML =usuario.login;
+    var eleEmail = document.getElementById('email');
+    eleEmail.innerHTML =usuario.email;
+    */
 popularTabela(Usuarios);
-}
+ }
 
 function popularTabela(Usuarios) {
     
