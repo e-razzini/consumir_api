8ed44d67072075bb0c53711bc73887b1abb5202f
Elton <eltonrazzini@hotmail.com>
2020-09-24 22:14:57 -0300

    acertos retirar ifs em ecesso

--- js/UsuarioView.js
+++ js/UsuarioView.js
@@ -33,7 +33,7 @@ function inserirLinhaTabela(usuario){
 }
 function validarUsuario(acao){
 
-var nome =document.getElementById('nome');
+var nome  =document.getElementById('nome');
 var email =document.getElementById('email');
 var login =document.getElementById('login');
 var senha =document.getElementById('senha');
@@ -49,46 +49,39 @@ if(acao == "add"){
     */
    if(nome.value == "" || email.value == "" || login.senha == "" ){
 
-       dadosValidos=false;
-       alert("email, nome ou login sem preencher");
-    }
+        dadosValidos=false;
+        alert("email, nome ou login sem preencher");
+        }
     
-    if(senha.value == "" ){
+        if (senha.value == "" || senhaTxt.length < 6) {
 
-        dadosValidos=false;
-        alert("Senha esta vazia.");
+        dadosValidos = false;
+        alert("Senha esta vazia,no minimo 6 caracteres.");
         var senhaTxt = senha.value;
-    }
-     if(senhaTxt.length < 6){
-       // var senhaTxt = senha.value;
-        dadosValidos=false;
-        alert("Senha precisa no minimo 6 caracteres.");
-
-    }
-     if(senhaValidar.value != senha.value){
+        }
+        if(senhaValidar.value != senha.value){
 
         dadosValidos=false;
         alert("senhas nao correspondem.");
 
         }
     
+        if(dadosValidos){
+        //ENVIAR PARA API
+        var objUsuario = {
+        "nome":nome.value,
+        "email":email.value,
+        "login":login.value,
+        "senha":senha.value
+        };
+        alert("usuario enviado");
+        //adiciona novo usuario
+        //adicionarUsuario(objUsuario);
+        }
 
-if(dadosValidos){
-    //ENVIAR PARA API
-var objUsuario = {
-    "nome":nome.value,
-    "email":email.value,
-    "login":login.value,
-    "senha":senha.value
-};
-alert("usuario enviado");
-//adiciona novo usuario
-//adicionarUsuario(objUsuario);
-}
-   
-}
-return false;
-}
+        }
+        return false;
+        }
 
 
 window.onload = function (){
