357cd1a0866ca313f0c7b8daeb925806cf71ec68
Elton <eltonrazzini@hotmail.com>
2020-09-26 16:57:42 -0300

    metodos put e delete - js button

--- js/UsuarioApi.js
+++ js/UsuarioApi.js
@@ -43,3 +43,42 @@ xhr.open("POST",url);
 xhr.send(JSON.stringify(objUsuario));
 
 }
+function editarUsuario(codigo,objUsuario) {
+    
+    let json = JSON.stringify(objUsuario);
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("PUT", url + '/codigo =' + codigo, true);
+    
+    xhr.setRequestHeader('content-type','application/json;charset=utf-8');
+    xhr.onload = function () {
+        
+        var usuario = JSON.parse(xhr.responseText);
+        if (this.readyState === 4 ) {
+            
+            exibirUsuarios(usuarios);
+        } else {
+            console.log("error");
+        }
+    }
+    
+    xhr.send(json);
+}
+function deletarUsuario(codigo) {
+    
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("DELETE", url + '/codigo=' + codigo, true);
+    
+    xhr.onload = function () {
+        var usuario = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 ) {
+
+            exibirUsuarios(usuarios);
+        } else {
+            console.log('erro');
+        }
+    }
+    xhr.send(null);
+}
--- js/UsuarioView.js
+++ js/UsuarioView.js
@@ -1,88 +1,111 @@
 function exibirUsuarios(Usuarios){
  
-popularTabela(Usuarios);
+    popularTabela(Usuarios);
+    // this.btn();
  }
+var b = document.createElement("btn");
+
+
 
 function popularTabela(Usuarios) {
     
     for(let i = 0; i < Usuarios.length; i++){
         inserirLinhaTabela(Usuarios[i]);
+        
+        this.btn();
     }
+}
+function btn() {
+    
+    var button = document.createElement('button');
+    button.setAttribute('type', 'button')
+    button.appendChild(document.createTextNode('delete'));
+    document.body.appendChild(button);
 
+    var buttonEdit = document.createElement('button');
+    buttonEdit.setAttribute('type','button')
+    buttonEdit.appendChild(document.createTextNode('editar'));
+    document.body.appendChild(buttonEdit);
+
+   
 }
 
 function inserirLinhaTabela(usuario){
 
-   var tabela = document.getElementById('tabelaUsuarios');
-   var numLinhas =tabela.rows.length;
-   var novaLinha = tabela.insertRow(numLinhas);
+        var tabela = document.getElementById('tabelaUsuarios');
+        var numLinhas =tabela.rows.length;
+        var novaLinha = tabela.insertRow(numLinhas);
 
-   var celCodigo = novaLinha.insertCell(0);
-   celCodigo.innerHTML = usuario.codigo;
+        var celCodigo = novaLinha.insertCell(0);
+        celCodigo.innerHTML = usuario.codigo;
 
-   var celNome = novaLinha.insertCell(1);
-   celNome.innerHTML = usuario.nome;
+        var celNome = novaLinha.insertCell(1);
+        celNome.innerHTML = usuario.nome;
 
-   var celEmail = novaLinha.insertCell(2);
-   celEmail.innerHTML = usuario.email;
+        var celEmail = novaLinha.insertCell(2);
+        celEmail.innerHTML = usuario.email;
 
-   var celLogin = novaLinha.insertCell(3);
-   celLogin.innerHTML = usuario.login;
+        var celLogin = novaLinha.insertCell(3);
+        celLogin.innerHTML = usuario.login;
+        
    
-   
-}
+  }
 function validarUsuario(acao){
 
-var nome  =document.getElementById('nome');
-var email =document.getElementById('email');
-var login =document.getElementById('login');
-var senha =document.getElementById('senha');
-var senhaValidar =document.getElementById('senhaValidar');
+    var nome  =document.getElementById('nome');
+    var email =document.getElementById('email');
+    var login =document.getElementById('login');
+    var senha =document.getElementById('senha');
+    var senhaValidar =document.getElementById('senhaValidar');
 
-if(acao == "add"){
+    if(acao == "add"){
 
-   var dadosValidos = true;
-   var senhaTxt = senha.value;
-   /*força o dado para  outro determinado dado
-   var senhaInt =parseInt(senha.value);
-   var senhaFloat =parseFloat(senha.value);
+    /*força o dado para  outro determinado dado
+    var senhaInt =parseInt(senha.value);
+    var senhaFloat =parseFloat(senha.value);
     */
-   if(nome.value == "" || email.value == "" || login.senha == "" ){
+    var dadosValidos = true;
+    var senhaTxt = senha.value;
+    if(nome.value == "" || email.value == "" || login.senha == "" ){
 
-        dadosValidos=false;
-        alert("email, nome ou login sem preencher");
-        }
+    dadosValidos=false;
+    alert("email, nome ou login sem preencher");
+    }
+
+    if (senha.value == "" || senhaTxt.length < 6) {
+
+    dadosValidos = false;
+    alert("Senha esta vazia,no minimo 6 caracteres.");
+    var senhaTxt = senha.value;
+    }
+    if(senhaValidar.value != senha.value){
+
+    dadosValidos=false;
+    alert("senhas nao correspondem.");
+
+    }
+
+    if(dadosValidos){
+    //ENVIAR PARA API
+    var objUsuario = {
+    "nome":nome.value,
+    "email":email.value,
+    "login":login.value,
+    "senha":senha.value
+    };
+    alert("usuario enviado");
+    //adiciona novo usuario
+    //adicionarUsuario(objUsuario);
+}
+
+}
+return false;
+}
+function editarUser(acao) {
     
-        if (senha.value == "" || senhaTxt.length < 6) {
+  
 
-        dadosValidos = false;
-        alert("Senha esta vazia,no minimo 6 caracteres.");
-        var senhaTxt = senha.value;
-        }
-        if(senhaValidar.value != senha.value){
-
-        dadosValidos=false;
-        alert("senhas nao correspondem.");
-
-        }
-    
-        if(dadosValidos){
-        //ENVIAR PARA API
-        var objUsuario = {
-        "nome":nome.value,
-        "email":email.value,
-        "login":login.value,
-        "senha":senha.value
-        };
-        alert("usuario enviado");
-        //adiciona novo usuario
-        //adicionarUsuario(objUsuario);
-        }
-
-        }
-        return false;
-        }
-
+}
 
 window.onload = function (){
     getUsuario();
